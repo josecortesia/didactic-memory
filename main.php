86b731680326c38169aceda4c6475fca95cdc704
Jose Cortesia <josegregoriocortesiarojas@gmail.com>
2021-05-06 17:05:19 -0400

    arreglo home

--- main.php
+++ main.php
@@ -52,7 +52,7 @@ if (!$_SESSION['autorizado']) {
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href="main.php" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Soporte</a>
@@ -101,7 +101,7 @@ if (!$_SESSION['autorizado']) {
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index.php" class="brand-link">
+    <a href="main.php" class="brand-link">
       
       <img src="dist/img/logo-third-eye-bueno.png" alt="ThirdEye" class="brand-image">
     </a>
